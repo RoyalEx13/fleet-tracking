a51c6d159c6fbb5a5ea637d30312a1173a5c8d46
RoyalEx13 <nonty009@hotmail.com>
2026-03-04 04:06:57 +0700

    feat: implement multi-vehicle GPS simulation

--- gateway-go/cmd/simulator/main.go
+++ gateway-go/cmd/simulator/main.go
@@ -7,6 +7,8 @@ import (
 	"math"
 	"net/http"
 	"os"
+	"strings"
+	"sync"
 	"time"
 )
 
@@ -18,34 +20,53 @@ type RawGPSData struct {
 }
 
 func main() {
-	GPSID := getEnv("GPS_IDS", "Tesxt_123")
+	rawIDs := getEnv("GPS_IDS", "test-001,test,002,test-003")
+	ids := strings.Split(rawIDs, ",")
+	fmt.Println("IDs List:", rawIDs)
 	targetURL := getEnv("GATEWAY_BASE_URL", "http://gateway:8080") + "/track"
 
-	fmt.Printf("Starting Simulator for Vehicle: %s\n", GPSID)
+	fmt.Printf("Starting Multi-Simulator for %d vehicles\n", len(ids))
 	fmt.Printf("Target Gateway: %s\n", targetURL)
 
-	lat, lon := 13.7563, 100.5018
-	angle := 0.0
+	var wg sync.WaitGroup
+
+	for i, id := range ids {
+		wg.Add(1)
+		vehicleID := strings.TrimSpace(id)
+
+		go simulateVehicle(vehicleID, targetURL, float64(i), &wg)
+	}
+
+	wg.Wait()
+}
+
+func simulateVehicle(id string, targetURL string, offset float64, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	lat, lon := 13.7563+(offset*0.002), 100.5018+(offset*0.002)
+	angle := offset
+
+	fmt.Printf("[%s] Simulator started...\n", id)
 
 	for {
 		angle += 0.1
-		lat += math.Sin(angle) * 0.0002
-		lon += math.Cos(angle) * 0.0002
+		currentLat := lat + math.Sin(angle)*0.0005
+		currentLon := lon + math.Cos(angle)*0.0005
 
 		data := RawGPSData{
-			ID:    GPSID,
-			Lat:   lat,
-			Lon:   lon,
-			Speed: 40.0 + (math.Sin(angle) * 10),
+			ID:    id,
+			Lat:   currentLat,
+			Lon:   currentLon,
+			Speed: 30.0 + (math.Sin(angle) * 15),
 		}
 
 		payload, _ := json.Marshal(data)
 		resp, err := http.Post(targetURL, "application/json", bytes.NewBuffer(payload))
 
 		if err != nil {
-			fmt.Println("Gateway connection error:", err)
+			fmt.Printf("[%s] Connection Error: %v\n", id, err)
 		} else {
-			fmt.Printf("Sent: Lat %.5f, Lon %.5f | Status: %d\n", lat, lon, resp.StatusCode)
+			fmt.Printf("[%s] Sent | Status: %d | Lat: %.5f, Lon: %.5f\n", id, resp.StatusCode, currentLat, currentLon)
 			resp.Body.Close()
 		}
 
